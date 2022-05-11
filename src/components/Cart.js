dd85ca1c98b99cfb38c51d9dec37ca2d39f0f11f
DokJS <omarmbengue6919@gmail.com>
2022-05-11 09:55:45 +0000

    modify cart's return syntax

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,29 +1,29 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import '../styles/Cart.css';
 
 const Cart = () => {
-   const [isOpen, setIsOpen] = useState(true);
-   const [cart, setCart] = useState(10);
+  const [isOpen, setIsOpen] = useState(true);
+  const [cart, setCart] = useState(10);
 
-   if(isOpen){
-    return (
-      <div className='lmj-cart'>
-        <button onClick={()=>setIsOpen(false)} className="lmj-cart-toggle-button">Fermer</button>
-          <h2>Panier</h2>
-          <ul>
-            
-          </ul>
-          <span>Total : {cart} $</span>
-          <button onClick={()=>setCart(0)}>Vider le panier</button>
-      </div>
-    )
-   }else {
-     return(
-       <div className="lmj-cart-closed">
-         <button onClick={()=>setIsOpen(true)} className="lmj-cart-toggle-button">Ouvrir le panier</button>
-       </div>
-     )
-   }
+
+  return (
+    isOpen ? (<div className='lmj-cart'>
+      <button onClick={() => setIsOpen(false)} className="lmj-cart-toggle-button">Fermer</button>
+      <h2>Panier</h2>
+      <ul>
+
+      </ul>
+      <span>Total : {cart} $</span>
+      <button onClick={() => setCart(0)}>Vider le panier</button>
+    </div>)
+      : (
+        <div className="lmj-cart-closed">
+          <button onClick={() => setIsOpen(true)} className="lmj-cart-toggle-button">Ouvrir le panier</button>
+        </div>
+      )
+  )
+
+
 }
 
 export default Cart
