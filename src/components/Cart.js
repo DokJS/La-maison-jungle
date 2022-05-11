9dd335943743b8febdc03222be99f2073f93aee4
DokJS <omarmbengue6919@gmail.com>
2022-05-11 19:58:13 +0000

    save cart into localStorage

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,37 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Cart.css';
 
 const Cart = (props) => {
   const [isOpen, setIsOpen] = useState(true);
   const { cart, setCart } = props;
+  // total cart
+  let total = 0;
+  cart.length > 0 && (total = cart.reduce((acc, current) => {
+    return (acc += current.price * current.quantity)
+  }, 0))
+
+  // title
+  useEffect(() => {
+    document.title = `LMJ: ${total} à payer`
+  }, [total])
+
+ // save cart content into localStorage
+useEffect(()=>{
+  if(cart.length === 0){
+    localStorage.setItem('cart',localStorage.getItem('cart'));
+  }else {
+    localStorage.setItem('cart',JSON.stringify(cart));
+  }
+},[cart]);
+
+// retrieve data from localStorage
+useEffect(()=>{
+  setCart( JSON.parse(localStorage.getItem('cart')))
+},[])
 
   let articles = null;
-   // articles display
+  // articles display
   // This let to map only if cart is'nt empty
-  cart.length > 0 && ( articles = cart.map(({ name, price, quantity }, index) => {
-    return (<div key={index} style={{fontWeight:'bold',marginBottom:'5px'}}>
+  cart.length > 0 && (articles = cart.map(({ name, price, quantity }, index) => {
+    return (<div key={index} style={{ fontWeight: 'bold', marginBottom: '5px' }}>
       {`${name.toUpperCase()}         x${quantity}        ${quantity * price}$`}
     </div>)
   }))
 
-  // total cart
-  let total = 0;
-  cart.length > 0 && ( total = cart.reduce( (acc,current) => {
-    return(acc+=current.price * current.quantity)
-  },0))
+  const resetCart = ()=>{
+    setCart([]);
+    localStorage.clear() // reset localStorage
+  }
+
   return (
     isOpen ? (<div className='lmj-cart'>
       <button onClick={() => setIsOpen(false)} className="lmj-cart-toggle-button">Fermer</button>
       <h2>Panier</h2>
-      <ul style={{paddingLeft:'0px'}}>
+      <ul style={{ paddingLeft: '0px' }}>
         {articles}
       </ul>
       <span>Total :{total} $</span>
-      <button onClick={() => setCart([])}>Vider le panier</button>
+      <button onClick={resetCart}>Vider le panier</button>
     </div>)
       : (
         <div className="lmj-cart-closed">
-          <button onClick={() => setIsOpen(true)} className="lmj-cart-toggle-button">Ouvrir le panier</button>
+          <button onClick={()=>setIsOpen(true)} className="lmj-cart-toggle-button">Ouvrir le panier</button>
         </div>
       )
   )
@@ -39,4 +63,4 @@ const Cart = (props) => {
 
 }
 
-export default Cart
+export default React.memo(Cart);
